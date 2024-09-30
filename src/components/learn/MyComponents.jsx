5bb0170affea040bbe8b050af5282d1da37c30c3
toantqph38960 <quavang2k4@gmail.com>
2024-09-30 22:27:25 +0700

    v30

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,8 +1,9 @@
-const MyComponent = ()=>{
-    return (
-      <div>
-        <h2>Hello, Quang Toản</h2>
-      </div>
-    );
-  }
+import './style.css';
+const MyComponent = () => {
+  return (
+    <>
+      <h2 className='child' style={{borderRadius:"10px"}}>Hello, Quang Toản</h2>
+    </>
+  );
+}
 export default MyComponent;
