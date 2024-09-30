1a54ec92c3705f836e56b14997e6c652bea52569
toantqph38960 <quavang2k4@gmail.com>
2024-09-30 22:59:20 +0700

    v31

--- src/components/learn/MyComponents.jsx
+++ src/components/learn/MyComponents.jsx
@@ -1,8 +1,18 @@
 import './style.css';
 const MyComponent = () => {
+  // const name = "Quang Toản"
+  // const age = 25
+  // const array = [1, 2,3,4];
+  const ob = {
+    name: 'Quang Toản',
+    age: 25,
+    address: 'Hà Nội',
+  }
   return (
     <>
-      <h2 className='child' style={{borderRadius:"10px"}}>Hello, Quang Toản</h2>
+      <h2 className='child'> {ob.name}</h2>
+      <h2 className='child'> {JSON.stringify(ob)}</h2>
+
     </>
   );
 }
