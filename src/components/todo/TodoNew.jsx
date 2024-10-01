27cd96d87e5df11b6f42c339a20b356a72842603
toantqph38960 <quavang2k4@gmail.com>
2024-10-01 14:27:14 +0700

    v36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,19 @@
 const TodoNew = (props) => {
     console.log(props);
-    const {addNewTodo} = props;
-    addNewTodo("quang toản");
+    const { addNewTodo } = props;
+    // addNewTodo("quang toản");
+    const handleClick = () => {
+        alert("click");
+    }
+    const handleOnchange = (name) => {
+        console.log("change", name);
+    }
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnchange(event.target.value)}
+            />
+            <button style={{ cursor: "pointer" }} onClick={handleClick}>Add</button>
         </div>
     )
 }
