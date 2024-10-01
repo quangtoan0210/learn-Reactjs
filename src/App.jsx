9bc3a3335061f1a26dddeeada481938893807392
toantqph38960 <quavang2k4@gmail.com>
2024-10-01 13:50:45 +0700

    v34

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import './components/todo/todo.css';
 import reactLogo from './assets/react.svg';
 
 const App = () => {
+  const name = 'quangtoan';
+  const age = 13;
+  const person = {
+    name: 'Quang Toản',
+    age: 25,
+    address: 'Hà Nội',
+  }
 
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData 
+      name={name}
+      age={age}
+      person={person}  // Truyền object person qua props cho component TodoData
+      />
       <div className='todo-image'>
         <img src={reactLogo} className='logo' />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,9 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    const {name,age,person} = props;
+    console.log(props);
     return (
         <div className='todo-data'>
+            <div>My name is {name}</div>
             <div> Learning React </div>
             <div> Watching Youtube</div>
         </div>
