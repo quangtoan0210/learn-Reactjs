1d7c969c4f3e4364a3624569d042ca2ea1134a9e
toantqph38960 <quavang2k4@gmail.com>
2024-10-01 21:08:01 +0700

    v66

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,6 +1,16 @@
-const UserPage = ()=>{
-    return(
-        <div>Users Page</div>
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
+
+const UserPage = () => {
+    return (
+        <>
+            <div style={{ padding: "20px" }}>
+                <UserForm />
+                <UserTable />
+            </div>
+
+        </>
+
     )
 }
 export default UserPage;
