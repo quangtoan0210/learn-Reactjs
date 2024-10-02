397d05941b3b0a06a37303e0624a68274ac97d8b
toantqph38960 <quavang2k4@gmail.com>
2024-10-02 10:55:53 +0700

    v77

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,13 +1,42 @@
-import { Link, NavLink } from 'react-router-dom'
-import './header.css'
-const Header = () => {
-    return (
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/users">User</NavLink></li>
-            <li><NavLink to="/books">Books</NavLink></li>
-        </ul>
+import { Link, NavLink } from 'react-router-dom';
+import { Menu } from 'antd';
+import { UsergroupAddOutlined, HomeOutlined, AuditOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
+const Header = () => {
+    const [current, setCurrent] = useState('');
+    const onClick = (e) => {
+        setCurrent(e.key);
+    };
+
+    const items = [
+        {
+            label: <Link to={"/"}>Home</Link>,
+            key: 'home',
+            icon: <HomeOutlined />,
+        },
+        {
+            label: <Link to={"/users"}>Users</Link>,
+            key: 'users',
+            icon: <UsergroupAddOutlined />
+        },
+        {
+            label: <Link to={"/books"}>Books</Link>,
+            key: 'books',
+            icon: <AuditOutlined />,
+
+        },
+
+    ];
+
+    return (
+        <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items}
+        />
     )
 }
-export default Header
+
+export default Header;
--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,13 +1,24 @@
-import { Space, Table, Tag } from 'antd';
-
-
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Table } from 'antd';
+import UpdateUserModal from './update.user.modal';
+import { useState } from 'react';
 
 const UserTable = (props) => {
-    const{dataUsers} =props;
+    const { dataUsers } = props;
+
+    const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+
+    const [dataUpdate, setDataUpdate] = useState(null);
+
     const columns = [
         {
             title: 'Id',
             dataIndex: '_id',
+            render: (_, record) => {
+                return (
+                    <a href='#'>{record._id}</a>
+                )
+            }
         },
         {
             title: 'Full Name',
@@ -16,11 +27,38 @@ const UserTable = (props) => {
         {
             title: 'Email',
             dataIndex: 'email',
-        }
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <div style={{ display: "flex", gap: "20px" }}>
+                    <EditOutlined
+                        onClick={() => {
+                            setDataUpdate(record);
+                            setIsModalUpdateOpen(true);
+                        }}
+                        style={{ cursor: "pointer", color: "orange" }} />
+                    <DeleteOutlined style={{ cursor: "pointer", color: "red" }} />
+                </div>
+            ),
+        },
     ];
 
     return (
-        <Table columns={columns} dataSource={dataUsers} rowKey={"_id"} />
+        <>
+            <Table
+                columns={columns}
+                dataSource={dataUsers}
+                rowKey={"_id"}
+            />
+            <UpdateUserModal
+                isModalUpdateOpen={isModalUpdateOpen}
+                setIsModalUpdateOpen={setIsModalUpdateOpen}
+                dataUpdate={dataUpdate}
+                setDataUpdate={setDataUpdate}
+            />
+        </>
     )
 }
 
